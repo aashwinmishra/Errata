e2196d2a166e694a7ef81eadf6c59a3359bf537a
Aashwin Mishra <62309130+aashwinmishra@users.noreply.github.com>
2025-02-05 11:40:30 -0800

    Create clip.py

---
+++ StableDiffusion/clip.py
@@ -0,0 +1,55 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from attention import SelfAttention
+
+
+class CLIPEmbedding(nn.Module):
+  def __init__(self, n_vocab, n_embd, n_tokens):
+    super().__init__()
+    self.token_embedding = nn.Embedding(n_vocab, n_embd)
+    self.position_embedding = nn.Parameter(torch.zeros(n_tokens, n_embd))
+
+  def forward(self, tokens):
+    x = self.token_embedding(tokens)
+    return x + self.position_embedding
+
+
+class CLIPLayer(nn.Module):
+  def __init__(self, n_head, n_embd):
+    super().__init__()
+    self.layernorm_1 = nn.LayerNorm(n_embd)
+    self.attention = SelfAttention(n_head, n_embd)
+    self.layernorm_2 = nn.LayerNorm(n_embd)
+    self.linear_1 = nn.Linear(n_embd, 4*n_embd)
+    self.linear_2 = nn.Linear(4*n_embd, n_embd)
+
+  def forward(self, x):
+    residue = x
+    x = self.layernorm_1(x)
+    x = self.attention(x, causal_mask=True)
+    x += residue 
+    residue = x 
+    x = self.layernorm_2(x)
+    x = self.linear_1(x)
+    x = x * torch.sigmoid(1.702 * x) #Quick GeLU
+    x = self.linear_2(x)
+    return x + residue
+
+
+class CLIP(nn.Module):
+  def __init__(self):
+    self.embedding = CLIPEmbedding(49408, 768, 77)
+    self.layers = nn.Module([
+        CLIPLayer(12, 768) for _ in range(12)
+    ])
+    self.layernorm = nn.LayerNorm(768)
+
+  def forward(self, tokens):
+    tokens = tokens.type(torch.long)
+    state = self.embedding(tokens)
+    for layer in self.layers:
+      state = layer(state)
+    output = self.layernorm(state)
+    return output 
+
