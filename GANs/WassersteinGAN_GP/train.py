0075b2548e902239d9d93a0e81d9c686b50507cd
Aashwin Mishra <62309130+aashwinmishra@users.noreply.github.com>
2025-03-01 14:00:10 -0800

    Create train.py

---
+++ GANs/WassersteinGAN_GP/train.py
@@ -0,0 +1,32 @@
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+import torchvision
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+
+from model import Generator, Critic, initialize_weights
+from engine import train
+
+
+#====================================================================
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+latent_dim = 100
+num_epochs = 5
+features_d = 64
+features_g = 64
+channels_img = 1
+batch_size = 64
+image_size = 64
+critic_iterations = 5
+lambda_gp = 10
+#=====================================================================
+transform = transforms.Compose([transforms.Resize(image_size),
+                                transforms.ToTensor(),
+                                transforms.Normalize((0.5,),(0.5,))
+                                ])
+dataset = datasets.MNIST(root = 'data/', download = True, transform=transform)
+train_dl = DataLoader(dataset, batch_size=batch_size, drop_last=True, shuffle=True)
+gen = Generator(latent_dim, channels_img, features_g).to(device)
+critic = Critic(channels_img, features_d).to(device)
+train(critic, gen, train_dl, batch_size, latent_dim, num_epochs, critic_iterations, lambda_gp, device)
